0d27c6df6a5d0dada17ceac04fe31973f674fc1a
blattersturm <peachypies@protonmail.ch>
2022-07-08 12:59:33 +0200

    feat(blink/fileapi): `path` getter (from Electron)

--- third_party/blink/renderer/core/fileapi/file.h
+++ third_party/blink/renderer/core/fileapi/file.h
@@ -212,6 +212,9 @@ class CORE_EXPORT File final : public Blob {
   }
   const String& name() const { return name_; }
 
+  // Getter for the path IDL attribute.
+  const String& path() const { return GetPath(); }
+
   // Getter for the lastModified IDL attribute,
   // http://dev.w3.org/2006/webapi/FileAPI/#file-attrs
   int64_t lastModified() const;
